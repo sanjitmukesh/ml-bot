01d5dab1a7d3b262ec432bb63ac96ee0755c4101
Sanjit Mukesh <sanjitmukesh1@gmail.com>
2025-09-27 17:38:48 -0500

    update train.py

--- train.py
+++ train.py
@@ -6,4 +6,13 @@ df = df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1)
 df = df.rename(columns={'v1': 'label', 'v2': 'message'})
 df = df.replace({'v1': {'ham': 0, 'spam': 1}})
 
-print(df.head())
+# features: messages
+# targets: label
+X = df['message']
+y = df['label']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=15
+)
